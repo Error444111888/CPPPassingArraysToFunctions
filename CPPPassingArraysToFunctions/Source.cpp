0d37c33dc9f314f84f293dec5743d302e830b59b
error418 <error418@hotmail.com>
2018-11-24 20:10:50 -0800

    Final Commit

---
+++ CPPPassingArraysToFunctions/Source.cpp
@@ -0,0 +1,29 @@
+// Passing arrays to functions
+
+#include <iostream>
+
+using namespace std;
+
+// This is the prototype. You don't have to have this. You can just put the function above the main
+void TotalPrices(int TheArray[], int SizeOfArray);
+
+
+
+int main()
+{
+	int BTCPrices[5] = { 3000,4000,5000,6000,7000 };
+	int LTCPrices[4] = { 30,40,50,60 };
+
+	TotalPrices(LTCPrices, 4);
+
+	cin.get();
+}
+
+
+void TotalPrices(int TheArray[], int SizeOfArray)
+{
+	for (int x = 0; x < SizeOfArray; x++)
+	{
+		cout << TheArray[x] << endl;
+	}
+}
